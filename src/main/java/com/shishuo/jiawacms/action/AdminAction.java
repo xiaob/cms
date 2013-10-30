0a403ee2a3aeee077a5eb608ed1250ba4988a383
Zhang Jiale <zhangjiale@shishuo.com>
2013-10-30 14:32:29 +0800

    init

---
+++ src/main/java/com/shishuo/jiawacms/action/AdminAction.java
@@ -0,0 +1,38 @@
+/*
+ * 
+ *	Copyright © 2013 Changsha Shishuo Network Technology Co., Ltd. All rights reserved.
+ *	长沙市师说网络科技有限公司 版权所有
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *	 
+ *		http://www.shishuo.com/jiawacms/licenses
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package com.shishuo.jiawacms.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.shishuo.jiawacms.service.AdminService;
+
+@Controller
+public class AdminAction {
+
+	@Autowired
+	private AdminService adminService;
+	
+}
---
+++ src/main/java/com/shishuo/jiawacms/service/AdminService.java
@@ -0,0 +1,39 @@
+/*
+ * 
+ *	Copyright © 2013 Changsha Shishuo Network Technology Co., Ltd. All rights reserved.
+ *	长沙市师说网络科技有限公司 版权所有
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *	 
+ *		http://www.shishuo.com/jiawacms/licenses
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package com.shishuo.jiawacms.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shishuo.jiawacms.dao.AdminDao;
+import com.shishuo.jiawacms.entity.vo.AdminVo;
+
+@Service
+public class AdminService {
+
+	@Autowired//注解
+	private AdminDao adminDao;
+	
+	
+	public List<AdminVo> allAdmin(){
+		return adminDao.allAdmin();
+	}
+}
