d6a52e9a71834ad5d7c0e3e4c43c12018d282bca
Zhang Jiale <zhangjiale@shishuo.com>
2013-10-30 18:03:28 +0800

    test test

---
+++ src/test/java/com/shishuo/jiawacms/service/FileServiceTest.java
@@ -0,0 +1,39 @@
+package com.shishuo.jiawacms.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/applicationContext.xml"})
+public class FileServiceTest {
+
+	@Test
+	public void testGetFileById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFilePageByFoderId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteFileById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateFileById() {
+		fail("Not yet implemented");
+	}
+
+}
