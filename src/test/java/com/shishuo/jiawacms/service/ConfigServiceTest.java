a106f44d69389dc86ce42472cafba7bca7ff03d3
Herbert Yang <yanghaibo@shishuo.com>
2013-10-30 17:52:53 +0800

    增加测试方法

---
+++ src/test/java/com/shishuo/jiawacms/service/ConfigServiceTest.java
@@ -0,0 +1,34 @@
+package com.shishuo.jiawacms.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/applicationContext.xml"})
+public class ConfigServiceTest {
+
+	@Test
+	public void testAddConfig() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteConfigByKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdagteConfigByKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConfigPage() {
+		fail("Not yet implemented");
+	}
+
+}
