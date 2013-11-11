c0aacf1e81edd19152fc12e4bed6d32714b9e796
Herbert Yang <yanghaibo@shishuo.com>
2013-11-11 16:03:40 +0800

    本地修改

---
+++ src/main/java/com/shishuo/jiawacms/action/HomeAction.java
@@ -0,0 +1,29 @@
+package com.shishuo.jiawacms.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shishuo.jiawacms.constant.ConfigConstant;
+
+/**
+ * 首页
+ * 
+ * @author Herbert
+ * 
+ */
+@Controller
+public class HomeAction {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String home(HttpServletRequest request, ModelMap modelMap) {
+		try {
+			return ConfigConstant.getTemplatePath() + "/home";
+		} catch (Exception e) {
+			return ConfigConstant.getTemplatePath() + "/500";
+		}
+	}
+}
---
+++ src/main/java/com/shishuo/jiawacms/constant/ConfigConstant.java
@@ -0,0 +1,24 @@
+package com.shishuo.jiawacms.constant;
+
+/**
+ * 系统配置常量
+ * 
+ * @author Herbert
+ * 
+ */
+public class ConfigConstant {
+
+	/**
+	 * 默认的模板
+	 */
+	public static String DEFAUTL_TEMPLATE = "default";
+
+	/**
+	 * 得到当前模板路径
+	 * 
+	 * @return
+	 */
+	public static String getTemplatePath() {
+		return "/template/" + DEFAUTL_TEMPLATE;
+	}
+}
