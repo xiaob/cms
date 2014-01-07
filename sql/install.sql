e0ca996c01196119e1a597a221f3bde34a414e80
Herbert Yang <yanghaibo@shishuo.com>
2014-01-07 18:04:31 +0800

    更新sql

--- sql/install.sql
+++ sql/install.sql
@@ -7,22 +7,64 @@ CREATE TABLE `admin` (
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`adminId`),
   UNIQUE KEY `idx_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='管理员';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='管理员';
+
+CREATE TABLE `article` (
+  `articleId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '文件ID',
+  `firstFolderId` bigint(20) DEFAULT '1' COMMENT '目录ID',
+  `secondFolderId` bigint(20) DEFAULT '0',
+  `thirdFolderId` bigint(20) DEFAULT '0',
+  `fourthFolderId` bigint(20) DEFAULT '0',
+  `adminId` bigint(20) DEFAULT '0' COMMENT '管理员ID',
+  `name` varchar(200) DEFAULT '' COMMENT '文件名称',
+  `title` varchar(200) DEFAULT '',
+  `price` decimal(10,2) DEFAULT '0.00' COMMENT '价格，报价',
+  `realPrice` decimal(10,2) DEFAULT '0.00' COMMENT '实际价格',
+  `copyCount` int(11) DEFAULT '0' COMMENT '文件被copy的次数，相当于是商品的库存',
+  `content` text COMMENT '文件内容',
+  `description` text,
+  `viewCount` int(11) DEFAULT '0' COMMENT '浏览数',
+  `commentCount` int(11) DEFAULT '0' COMMENT '评论数',
+  `owner` varchar(20) DEFAULT 'app',
+  `type` varchar(20) DEFAULT 'article' COMMENT '文件类型：0 文章 1 照片 2 下载 3 商品',
+  `status` varchar(20) DEFAULT 'init' COMMENT '状态：0 隐藏 1 显示',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
+  `expireTime` datetime DEFAULT NULL COMMENT '过期时间',
+  PRIMARY KEY (`articleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COMMENT='文件';
+
+CREATE TABLE `attachment` (
+  `attachmentId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `kindId` bigint(20) DEFAULT '0',
+  `name` varchar(200) DEFAULT NULL,
+  `path` varchar(200) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `link` varchar(500) DEFAULT NULL,
+  `kind` varchar(20) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`attachmentId`)
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=ucs2;
 
 CREATE TABLE `comment` (
   `commentId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '评论ID',
   `userId` bigint(20) DEFAULT NULL COMMENT '用户ID',
   `fatherId` bigint(20) DEFAULT NULL COMMENT '父评论ID',
-  `fileId` bigint(20) DEFAULT NULL COMMENT '文件ID',
+  `kindId` bigint(20) DEFAULT NULL,
+  `kind` varchar(45) DEFAULT NULL COMMENT '文件ID',
   `name` varchar(45) DEFAULT NULL COMMENT '评论者',
   `email` varchar(45) DEFAULT NULL COMMENT '评论者邮件地址',
   `url` varchar(200) DEFAULT NULL COMMENT '评论者网址',
+  `phone` bigint(20) DEFAULT NULL,
   `content` text COMMENT '内容',
   `ip` varchar(45) DEFAULT NULL COMMENT 'Ip',
   `status` varchar(20) DEFAULT NULL COMMENT '状态',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`commentId`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COMMENT='评论';
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='评论';
 
 CREATE TABLE `config` (
   `key` varchar(45) NOT NULL COMMENT 'Key',
@@ -32,57 +74,29 @@ CREATE TABLE `config` (
   PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='网站配置';
 
-CREATE TABLE `file` (
-  `fileId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '文件ID',
-  `fatherId` bigint(20) DEFAULT NULL,
-  `folderId` bigint(20) DEFAULT NULL COMMENT '目录ID',
-  `adminId` bigint(20) DEFAULT NULL COMMENT '管理员ID',
-  `ename` varchar(200) DEFAULT NULL,
-  `name` varchar(200) DEFAULT NULL COMMENT '文件名称',
-  `title` varchar(200) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL COMMENT '密码',
-  `contentType` varchar(45) DEFAULT NULL COMMENT '内容类型，同HTTP协议中的Content_Type',
-  `size` int(11) DEFAULT NULL COMMENT '文件大小',
-  `price` decimal(10,2) DEFAULT NULL COMMENT '价格，报价',
-  `realPrice` decimal(10,2) DEFAULT NULL COMMENT '实际价格',
-  `copyCount` int(11) DEFAULT NULL COMMENT '文件被copy的次数，相当于是商品的库存',
-  `content` text COMMENT '文件内容',
-  `description` varchar(500) DEFAULT NULL,
-  `viewCount` int(11) DEFAULT NULL COMMENT '浏览数',
-  `commentCount` int(11) DEFAULT NULL COMMENT '评论数',
-  `type` varchar(20) DEFAULT NULL COMMENT '文件类型：0 文章 1 照片 2 下载 3 商品',
-  `status` varchar(20) DEFAULT NULL COMMENT '状态：0 隐藏 1 显示',
-  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
-  `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
-  `expireTime` datetime DEFAULT NULL COMMENT '过期时间',
-  PRIMARY KEY (`fileId`)
-) ENGINE=InnoDB AUTO_INCREMENT=266 DEFAULT CHARSET=utf8 COMMENT='文件';
-
 CREATE TABLE `folder` (
   `folderId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '目录ID',
-  `fatherId` bigint(20) DEFAULT NULL COMMENT '父亲Id，用于构建目录树',
-  `ename` varchar(45) DEFAULT NULL COMMENT '英文名',
-  `name` varchar(100) DEFAULT NULL COMMENT '中文名',
-  `path` varchar(200) DEFAULT NULL COMMENT '路径',
+  `fatherId` bigint(20) NOT NULL DEFAULT '0' COMMENT '父亲Id，用于构建目录树',
+  `ename` varchar(45) NOT NULL COMMENT '英文名',
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '中文名',
+  `path` varchar(200) NOT NULL DEFAULT '' COMMENT '路径',
   `content` text,
-  `level` tinyint(4) DEFAULT NULL COMMENT '层级',
-  `sort` tinyint(4) DEFAULT NULL COMMENT '排序',
-  `count` int(11) DEFAULT NULL COMMENT '文件数',
-  `rank` varchar(20) DEFAULT NULL COMMENT '等级',
-  `type` varchar(20) DEFAULT NULL COMMENT '目录类型：0 文章 1 照片 2 下载 3 商品',
-  `status` varchar(20) DEFAULT NULL COMMENT '状态：0 隐藏 1 现实',
+  `level` tinyint(4) DEFAULT '1' COMMENT '层级',
+  `sort` tinyint(4) DEFAULT '0' COMMENT '排序',
+  `count` int(11) DEFAULT '0' COMMENT '文件数',
+  `rank` varchar(20) DEFAULT 'everyone' COMMENT '等级',
+  `status` varchar(20) DEFAULT 'hidden' COMMENT '状态：0 隐藏 1 现实',
+  `owner` varchar(45) DEFAULT 'app',
   `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `updateTime` datetime DEFAULT NULL,
   PRIMARY KEY (`folderId`),
   UNIQUE KEY `ename_UNIQUE` (`ename`)
-) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8 COMMENT='目录';
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='目录';
 
-CREATE TABLE `log` (
-  `logId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '日志ID',
-  `content` text COMMENT '日志内容',
-  `level` varchar(20) DEFAULT NULL COMMENT '日志等级：0 DEBUG 1 INFO 2 WARN 3 ERROR 4 FATAL',
+CREATE TABLE `order` (
+  `orderId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '日志ID',
   `createTime` datetime DEFAULT NULL COMMENT '时间',
-  PRIMARY KEY (`logId`)
+  PRIMARY KEY (`orderId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='日志';
 
 CREATE TABLE `user` (
