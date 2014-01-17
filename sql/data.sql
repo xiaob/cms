505c4f9353bce80c0590fb1be9e93b2aac3df83c
Herbert Yang <yanghaibo@shishuo.com>
2014-01-17 17:38:47 +0800

    修改部署流程，更改默认模板为cms

--- sql/data.sql
+++ sql/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO `admin` VALUES (1,'cmstest@shishuo.com','cmstest','0bbc68ddb8ba8997c481534ded7cd82e','init','2012-08-08 00:00:00');
 INSERT INTO `folder` VALUES (1,0,'default','默认','1','',1,0,0,'everyone',NULL,'hidden','system','2012-08-08 00:00:00',NULL);
+INSERT INTO `config` (`key`, `value`, `description`, `createTime`) VALUES ('sys_sitename', '师说', '网站名字', '2012-08-08 00:00:00');
+INSERT INTO `config` (`key`, `value`, `description`, `createTime`) VALUES ('sys_sitedesc', '师者，传道授业解惑也', '网站描述 ', '2012-08-08 00:00:00');
+INSERT INTO `config` (`key`, `value`, `description`, `createTime`) VALUES ('sys_theme', 'cms', '网站模板', '2012-08-08 00:00:00');
